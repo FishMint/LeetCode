239070fc7670ec69980f635bbd35621c99411295
mac <dsr@mac>
2024-05-09 21:49:30 +0800

    a

---
+++ 182.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    string dynamicPassword(string password, int target) {
+		reverse(password.begin(), password.end());
+		reverse(password.begin(), password.end() - target);
+		reverse(password.end() - target, password.end());
+		return password;
+    }
+};
+
+int main(void){
+	Solution s;
+	cout<< s.dynamicPassword("s3cur1tyC0d3", 4)<<endl;
+	return 0;
+}
