9c548d730829d5dd9b63255628430c6aca49c84d
PC <dsr@dsr>
2024-05-17 22:11:27 +0800

    1

--- retry.cc
+++ retry.cc
@@ -3,25 +3,44 @@
 #include <algorithm>
 using namespace std;
 
-class Solution
-{
+struct ListNode {
+  int val;
+  ListNode *next;
+};
+
+class Solution {
 public:
-    // 双指针法
-    int removeElement(vector<int> &nums, int val)
-    {
-        int slow = 0, fast = 0;
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int lenA = 0, lenB = 0;
+        ListNode *listA, *listB;
 
-        for(; fast < nums.size(); fast++) {
-            if(nums[fast] != val) {
-                nums[slow++] = nums[fast];
-            }
+        //计算A，B的长度
+        for(listA = headA; listA != NULL; listA = listA->next) lenA++;
+        for(listB = headB; listB != NULL; listB = listB->next) lenB++;
+        
+        //让A始终是最长的
+        if(lenA < lenB) {
+            swap(headA, headB);
+            swap(lenA, lenB);
+        } 
+
+        listA = headA;
+        listB = headB;
+
+        for(int i = 0; i < lenA - lenB; i++) listA = listA->next;
+
+        while (listA != listB) {
+            if(listA == NULL || listB == NULL)  return NULL;
+            listA = listA->next;
+            listB = listB->next;
         }
-        return slow;
 
+        return listA;
     }
 };
 
 int main()
+
 {
     
     return 0;
